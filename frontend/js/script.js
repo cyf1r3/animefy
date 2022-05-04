7c1d69fca43576b747539f4250b73f7d0b55532c
Anant <anantlakhan15@gmail.com>
2022-05-05 04:23:25 +0530

    All song funtionalities working now

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -10,20 +10,33 @@ let masterSongName = document.getElementById('masterSongName');
 let songItems = Array.from(document.getElementsByClassName('songItem'));
 
 let songs = [
-    {songName: "Naruto shippuden opening 14", filePath: "../../res/songs/1.mp3", coverPath: "../../res/covers/1.jpg"},
-    {songName: "Itachi Uchiha-Theme song", filePath: "../../res/songs/2.mp3", coverPath: "../../res/covers/2.jpg"},
-    {songName: "Naruto-Loneliness", filePath: "../../res/songs/3.mp3", coverPath: "../../res/covers/3.jpg"},
-    {songName: "Naruto OST-Confrontment", filePath: "../../res/songs/4.mp3", coverPath: "../../res/covers/4.jpg"},
-    {songName: "Naruto OST-Survival Examination", filePath: "../../res/songs/5.mp3", coverPath: "../../res/covers/5.jpg"},
-    {songName: "Naruto Shippudden OST-Shisui's Theme ", filePath: "../../res/songs/6.mp3", coverPath: "covers/6.jpg"},
-    {songName: "Naruto OST- Haku and Zabuza's Theme", filePath: "../../res/songs/7.mp3", coverPath: "../../res/covers/7.jpg"},
-    {songName: "Naruto opening song 1", filePath: "../../res/songs/8.mp3", coverPath: "../../res/covers/8.jpg"},
-    {songName: "Naruto Shippudden OST-Pein's Theme", filePath: "../../res/songs/9.mp3", coverPath: "../../res/covers/9.jpg"},
-    {songName: "Naruto Shippudden OST-Sasuke's Theme", filePath: "../../res/songs/10.mp3", coverPath: "c../../res/overs/10.jpg"},
-    {songName: "Naruto Shippudden OST-Hebi and Taka", filePath: "../../res/songs/11.mp3", coverPath: "../../res/covers/1.jpg"},
-    {songName: "Naruto OST-Sasuke's Theme", filePath: "../../res/songs/12.mp3", coverPath: "../../res/covers/2.jpg"},
-    {songName: "Naruto Shippuden OST 3 Track 04", filePath: "../../res/songs/13.mp3", coverPath: "../../res/covers/3.jpg"},
-    {songName: "Naruto Shippuden OST I-Man of the World", filePath: "../../res/songs/14.mp3", coverPath: "../../res/covers/4.jpg"},
+    {songName: "Naruto shippuden opening 14", filePath: "../res/songs/1.mp3", coverPath: "../res/covers/1.jpg"},
+
+    {songName: "Itachi Uchiha-Theme song", filePath: "../res/songs/2.mp3", coverPath: "../res/covers/2.jpg"},
+
+    {songName: "Naruto-Loneliness", filePath: "../res/songs/3.mp3", coverPath: "../res/covers/3.jpg"},
+
+    {songName: "Naruto OST-Confrontment", filePath: "../res/songs/4.mp3", coverPath: "../res/covers/4.jpg"},
+
+    {songName: "Naruto OST-Survival Examination", filePath: "../res/songs/5.mp3", coverPath: "../res/covers/5.jpg"},
+
+    {songName: "Naruto Shippudden OST-Shisui's Theme ", filePath: "../res/songs/6.mp3", coverPath: "../res/covers/6.jpg"},
+
+    {songName: "Naruto OST- Haku and Zabuza's Theme", filePath: "../res/songs/7.mp3", coverPath: "../res/covers/7.jpg"},
+
+    {songName: "Naruto opening song 1", filePath: "../res/songs/8.mp3", coverPath: "../res/covers/8.jpg"},
+
+    {songName: "Naruto Shippudden OST-Pein's Theme", filePath: "../res/songs/9.mp3", coverPath: "../res/covers/9.jpg"},
+
+    {songName: "Naruto Shippudden OST-Sasuke's Theme", filePath: "../res/songs/10.mp3", coverPath: "../res/covers/10.jpg"},
+
+    {songName: "Naruto Shippudden OST-Hebi and Taka", filePath: "../res/songs/11.mp3", coverPath: "../res/covers/1.jpg"},
+
+    {songName: "Naruto OST-Sasuke's Theme", filePath: "../res/songs/12.mp3", coverPath: "../res/covers/2.jpg"},
+
+    {songName: "Naruto Shippuden OST 3 Track 04", filePath: "../res/songs/13.mp3", coverPath: "../res/covers/3.jpg"},
+
+    {songName: "Naruto Shippuden OST I-Man of the World", filePath: "../res/songs/14.mp3", coverPath: "../res/covers/4.jpg"},
 ]
 
 songItems.forEach((element, i)=>{ 
@@ -71,7 +84,7 @@ Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
         songIndex = parseInt(e.target.id);
         e.target.classList.remove('fa-play-circle');
         e.target.classList.add('fa-pause-circle');
-        audioElement.src = `../../res/songs/${songIndex+1}.mp3`;
+        audioElement.src = `../res/songs/${songIndex+1}.mp3`;
         masterSongName.innerText = songs[songIndex].songName;
         audioElement.currentTime = 0;
         audioElement.play();
@@ -100,7 +113,7 @@ document.getElementById('next').addEventListener('click', ()=>{
 */
 
 document.getElementById('next').addEventListener('click', ()=>{
-    //audioElement.src = `../../res/songs/${songIndex}.mp3`;
+    //audioElement.src = `../res/songs/${songIndex}.mp3`;
     masterSongName.innerText = songs[songIndex].songName;
     audioElement.currentTime += 10;
     audioElement.play();
@@ -115,7 +128,7 @@ document.getElementById('previous').addEventListener('click', ()=>{
     else{
         songIndex -= 1;
     }
-    audioElement.src = `../../res/songs/${songIndex+1}.mp3`;
+    audioElement.src = `../res/songs/${songIndex+1}.mp3`;
     masterSongName.innerText = songs[songIndex].songName;
     audioElement.currentTime = 0;
     audioElement.play();
